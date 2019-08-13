23840792ae390c3aa1bd0a854dc104bcfcd346bd
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-13 17:45:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase = phrase
-    return "HUH?! SPEAK UP, SONNY!"
+  if phrase = phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
 end
   
   
